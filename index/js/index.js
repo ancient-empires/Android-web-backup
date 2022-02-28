8f0798be227496248600456ed58f812a36c6fa0f
ytx21cn <5900105+ytx21cn@users.noreply.github.com>
2022-02-28 13:01:09 -0800

    initialize main UI and fullscreen settings in index.html

--- index/js/index.js
+++ index/js/index.js
@@ -6,61 +6,74 @@ import initFullscreenSettings, { getFullscreenStatus, setFullscreenStatus } from
 
 import { MAIN_ID, FULLSCREEN_SETTINGS_TOGGLE_ID } from "./key_element_ids.js";
 
-const init = () => {
-    (() => {
-        // check browser support
-        const browserIsSupported = checkBrowserSupport();
+/**
+ * Initialize the UI for index.html.
+ * First check browser support.
+ * Then show main element if the browser supports running the game.
+ * @returns { boolean }
+ */
+const initIndexUi = () => {
+    // check browser support
+    const browserIsSupported = checkBrowserSupport();
 
-        // if browser is supported, then show main element
-        if (browserIsSupported) {
-            const mainElement = document.getElementById(MAIN_ID);
-            const mainElementObserver = new class MainElementObserver extends Observer {
-                /** @param { HTMLElement } mainElement */
-                constructor(mainElement) {
-                    super();
-                    this.mainElement = mainElement;
-                }
-
-                /** @override */
-                receive(value) {
-                    mainElement.hidden = !value;
-                }
-            }(mainElement);
-
-            initMainUi(browserIsSupported, mainElementObserver);
+    // if browser is supported, then show main element
+    // otherwise, hide the main element
+    const mainElement = document.getElementById(MAIN_ID);
+    const mainElementObserver = new class MainElementObserver extends Observer {
+        /** @param { HTMLElement } mainElement */
+        constructor(mainElement) {
+            super();
+            this.mainElement = mainElement;
         }
 
-        return browserIsSupported;
-    })() && (() => {
-        // initialize fullscreen settings
+        /** @override */
+        receive(value) {
+            mainElement.hidden = !value;
+        }
+    }(mainElement);
 
-        // use the checkbox to set fullscreen status
-        const checkbox = document.getElementById(FULLSCREEN_SETTINGS_TOGGLE_ID);
-        const checkboxObserver = new class FullscreenCheckboxObserver extends Observer {
-            /** @param { HTMLInputElement } checkbox */
-            constructor(checkbox) {
-                super();
+    initMainUi(browserIsSupported, mainElementObserver);
 
-                this.checkbox = checkbox;
-
-                checkbox.checked = getFullscreenStatus();
-                checkbox.addEventListener("input", FullscreenCheckboxObserver.inputListener);
-            }
-
-            /** @override */
-            receive(value) {
-                this.checkbox.checked = Boolean(value);
-            }
-
-            static inputListener(e) {
-                setFullscreenStatus(Boolean(e.target.checked));
-            }
-        }(checkbox);
-
-        initFullscreenSettings(checkboxObserver);
-
-        return true;
-    })();
+    return browserIsSupported;
 };
 
-document.addEventListener("DOMContentLoaded", init);
+class FullscreenCheckboxObserver extends Observer {
+    /** @param { HTMLInputElement } checkbox */
+    constructor(checkbox) {
+        super();
+
+        this.checkbox = checkbox;
+
+        checkbox.checked = getFullscreenStatus();
+        checkbox.addEventListener("input", FullscreenCheckboxObserver.inputListener);
+    }
+
+    /** @override */
+    receive(value) {
+        this.checkbox.checked = Boolean(value);
+    }
+
+    /** @param { Event } e */
+    static inputListener(e) {
+        setFullscreenStatus(Boolean(e.target.checked));
+    }
+}
+
+/**
+ * Initialize fullscreen settings.
+ * @returns { boolean }
+ */
+const initFullscreen = () => {
+    // use the checkbox on the page to set fullscreen status
+    const checkbox = document.getElementById(FULLSCREEN_SETTINGS_TOGGLE_ID);
+    // add a checkbox observer to sync with the page's fullscreen settings
+    const checkboxObserver = new FullscreenCheckboxObserver(checkbox);
+
+    initFullscreenSettings(checkboxObserver);
+
+    return true;
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+    return initIndexUi() && initFullscreen();
+});
