523b780f4c25b215325f651580817d9e00a2389c
ytx21cn <5900105+ytx21cn@users.noreply.github.com>
2022-03-01 22:08:32 -0800

    added "full-area" custom element

---
+++ index/js/custom_elements/full-area/full-area.js
@@ -0,0 +1,5 @@
+/** full-area custom element */
+export default class FullAreaElement extends HTMLElement {
+}
+
+customElements.define('full-area', FullAreaElement);
--- index/js/custom_elements/init.js
+++ index/js/custom_elements/init.js
@@ -1,4 +1,5 @@
 /** @module init Module to import all custom elements */
 
+import './full-area/full-area.js';
 import './full-page/full-page.js';
 import './full-page/full-page-popup/full-page-popup.js';
