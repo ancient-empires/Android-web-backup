9e6a75bf6cdc6aec551be77a2df4f5e106adcea8
ytx21cn <5900105+ytx21cn@users.noreply.github.com>
2022-03-01 23:08:16 -0800

    remove shadow DOM from full-page.js and full-page-popup.js

--- index/js/custom_elements/full-page/full-page-popup.js
+++ index/js/custom_elements/full-page/full-page-popup.js
@@ -1,10 +1,4 @@
-import {createShadow} from '../helper.js';
-
-const template = /* html */ `
-<slot><!-- Popup content --></slot>
-`;
-
-/** full-page custom element */
+/** <full-page-popup> custom element */
 export default class FullPagePopupElement extends HTMLElement {
   /** Initialize a full-page-popup element. */
   constructor() {
--- index/js/custom_elements/full-page/full-page.js
+++ index/js/custom_elements/full-page/full-page.js
@@ -1,16 +1,5 @@
-import {createShadow} from '../helper.js';
-
-const template = /* html */ `
-<slot><!-- Elements within the container --></slot>
-`;
-
-/** full-page custom element */
+/** <full-page> custom element */
 export default class FullPageElement extends HTMLElement {
-  /** Initialize a full-page element. */
-  constructor() {
-    super();
-    createShadow(this, 'closed', template);
-  }
 }
 
 customElements.define('full-page', FullPageElement);
