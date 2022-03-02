fd977258eb37f52e98fe65df43f7faec9e5a7362
ytx21cn <5900105+ytx21cn@users.noreply.github.com>
2022-03-01 23:16:55 -0800

    removed shadow DOM in full-page-popup.js

--- index/js/custom_elements/full-page/full-page-popup.js
+++ index/js/custom_elements/full-page/full-page-popup.js
@@ -1,10 +1,5 @@
 /** <full-page-popup> custom element */
 export default class FullPagePopupElement extends HTMLElement {
-  /** Initialize a full-page-popup element. */
-  constructor() {
-    super();
-    createShadow(this, 'closed', template);
-  }
 }
 
 customElements.define('full-page-popup', FullPagePopupElement);
