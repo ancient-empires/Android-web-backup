345e52fe5a3495875493018780cac6e05854c029
ytx21cn <5900105+ytx21cn@users.noreply.github.com>
2022-03-05 20:28:58 -0800

    added helper function to normalize a hash

--- index/js/helpers.js
+++ index/js/helpers.js
@@ -1,4 +1,30 @@
 /**
+ * Normalize a hash.
+ * For undefined, null, '', or '#':
+ * - If @param keepHashOnEmpty is true, then return '#'.
+ * - Otherwise return empty string.
+ *
+ * Otherwise, for a normal @param hash:
+ * - If hash starts with '#', return original hash.
+ * - Otherwise, add '#' at the beginning.
+ *
+ * @param { ?string | undefined } hash the hash to normalize.
+ * @param { boolean } keepHashOnEmpty whether to keep '#' for empty hash.
+ * @return { string } normalized hash.
+ */
+export const normalizeHash = (hash = '', keepHashOnEmpty = false) => {
+  switch (hash) {
+    case undefined:
+    case null:
+    case '':
+    case '#':
+      return keepHashOnEmpty ? '#' : '';
+    default:
+      return hash.startsWith('#') ? hash : `#${hash}`;
+  }
+};
+
+/**
  * Navigate to a specific hash on the current page.
  * If the hash is empty, then remove the `#` sign in the URL.
  *
@@ -8,15 +34,17 @@
  *   (Default is `false`)
  */
 export const navigateToHash = (hash = '', replace = false) => {
+  hash = normalizeHash(hash, true);
+
   if (replace) {
-    window.location.replace(`#${hash}`);
+    window.location.replace(hash);
   } else {
     window.location.hash = hash;
   }
 
-  if (hash === '') {
+  if (window.location.hash === '') {
     // remove the `#` sign in the URL
     window.history.replaceState(null, document.title,
-        document.location.pathname);
+        window.location.pathname);
   }
 };
