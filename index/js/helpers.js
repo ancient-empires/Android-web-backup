c7e2ef167bb3b216c82a95b8bf23d08e72d44523
ytx21cn <5900105+ytx21cn@users.noreply.github.com>
2022-03-06 21:37:28 -0800

    added helper functions to add and remove multiple elements from a set

--- index/js/helpers.js
+++ index/js/helpers.js
@@ -59,3 +59,23 @@ export const navigateToHash = (hash = '', replace = false) => {
 export const navigateToTopHash = (replace = false) => {
   navigateToHash('', replace);
 };
+
+/**
+ * Add provided elements to a set.
+ * @param { Set } set The set to add elements to.
+ * @param { ...any } elements Elements to add.
+*/
+export const addToSet = (set, ...elements) => {
+  elements.forEach((element) => set.add(element));
+};
+
+/**
+ * Remove provided elements from a set.
+ * Hint: Use removeFromSet(set, ...set) to remove all elements.
+ *
+ * @param { Set } set The set to remove elements from.
+ * @param { ...any } elements Elements to remove.
+ */
+export const removeFromSet = (set, ...elements) => {
+  elements.forEach((element) => set.delete(element));
+};
