16366253259e5c600c48653f19c707049e143d4d
ytx21cn <5900105+ytx21cn@users.noreply.github.com>
2022-03-06 22:51:48 -0800

    added tabbed content observer on <tab-content> element

--- index/js/custom_elements/main-ui/tab-content.js
+++ index/js/custom_elements/main-ui/tab-content.js
@@ -1,6 +1,46 @@
+import Observer from '../../observers/observer.js';
+import {getActiveTabContentId, addTabbedUiObservers, removeTabbedUiObservers}
+  from '../../observers/tabbed_ui.js';
+
 /** <tab-content> custom element */
 export default class TabContentElement extends HTMLElement {
   static tagName = 'tab-content';
+
+  /** Construct a <tab-content> element. */
+  constructor() {
+    super();
+
+    this.observer = new (class TabContentObserver extends Observer {
+      /** @param { TabContentElement } tabContentElement */
+      constructor(tabContentElement) {
+        super();
+        this.tabContentElement = tabContentElement;
+      }
+
+      /** @override */
+      receive(_activeTabContentId) {
+        this.tabContentElement.hidden = !this.tabContentElement.isActive();
+      }
+    })(this);
+  }
+
+  /**
+   * Check if the current tab is active.
+   * @return { boolean } `true` if this tab is active, `false` otherwise.
+   */
+  isActive() {
+    return getActiveTabContentId() === this.id;
+  }
+
+  /** Callback when the element is loaded into DOM. */
+  connectedCallback() {
+    addTabbedUiObservers(this.observer);
+  }
+
+  /** Callback when the element is removed from DOM. */
+  disconnectedCallback() {
+    removeTabbedUiObservers(this.observer);
+  }
 }
 
 customElements.define(TabContentElement.tagName, TabContentElement);
