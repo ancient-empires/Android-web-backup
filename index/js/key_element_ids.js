10078a75792036a9b6dfea3aa23ac4ccc545468d
ytx21cn <5900105+ytx21cn@users.noreply.github.com>
2022-03-06 22:33:42 -0800

    use buttons on the main page

--- index/js/key_element_ids.js
+++ index/js/key_element_ids.js
@@ -7,5 +7,7 @@ export const NOT_MOBILE_USER_AGENT_POPUP_ID =
 export const MAIN_ID = 'main';
 export const FULLSCREEN_SETTINGS_TOGGLE_ID =
   'fullscreen-settings-toggle';
+export const AE1_START_GAME_ID = 'ae1-start-game';
+export const AE2_START_GAME_ID = 'ae2-start-game';
 export const AE1_GAME_IFRAME_ID = 'ae1-game-iframe';
 export const AE2_GAME_IFRAME_ID = 'ae2-game-iframe';
