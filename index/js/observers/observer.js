369762e76d06ab295cd24743a1deb77548fbad6c
ytx21cn <5900105+ytx21cn@users.noreply.github.com>
2022-03-11 13:37:27 -0800

    added publishTo method to publish the value to a collection of observers

--- index/js/observers/observer.js
+++ index/js/observers/observer.js
@@ -5,4 +5,15 @@ export default class Observer {
    */
   receive(_value) {
   }
+
+  /**
+   * Publish a value for all observers in the specified array or set
+   * to receive.
+   * @param { Observer[] | Set<Observer> } observers
+   *   the collection of observers to receive the value.
+   * @param { any } value the value to receive.
+   */
+  static publishTo(observers, value) {
+    observers.forEach((observer) => observer.receive(value));
+  }
 }
