148096d895e9a01778abd5b4b709fb5467e35a37
ytx21cn <5900105+ytx21cn@users.noreply.github.com>
2022-03-11 11:25:10 -0800

    reorganized code to initialize index.html

--- index/js/index.js
+++ index/js/index.js
@@ -13,78 +13,70 @@ import {MAIN_ID, WEB_SQL_UNSUPPORTED_POPUP_ID,
   AE1_GAME_IFRAME_ID, AE2_GAME_IFRAME_ID}
   from './key_element_ids.js';
 
-/**
- * Initialize the UI for index.html.
- * First check browser support.
- * Then show main element if the browser supports running the game.
- *
- * @return { boolean } `true` if browser supports running the game,
- * `false` otherwise.
- */
-const initIndexUi = () => {
-  // check browser support
-  // set visibility of popups & main element
-  document.getElementById(WEB_SQL_UNSUPPORTED_POPUP_ID).hidden =
+/** Initialize the UI for index.html */
+const init = () => {
+  (() => {
+    // Step 1: Check browser support.
+
+    document.getElementById(WEB_SQL_UNSUPPORTED_POPUP_ID).hidden =
     hasWebSqlSupport();
-  document.getElementById(NOT_MOBILE_USER_AGENT_POPUP_ID).hidden =
+    document.getElementById(NOT_MOBILE_USER_AGENT_POPUP_ID).hidden =
     isMobile();
-  document.getElementById(MAIN_ID).hidden = !browserIsSupported();
 
-  // initialize observers for AE1 and AE2
-  const ae1IframeObserver = new GameIframeObserver(
-      document.getElementById(AE1_GAME_IFRAME_ID),
-      GAME_URLS[GAMES.AE1]);
-  const ae2IframeObserver = new GameIframeObserver(
-      document.getElementById(AE2_GAME_IFRAME_ID),
-      GAME_URLS[GAMES.AE2]);
-  initGameIframeObservers(ae1IframeObserver, ae2IframeObserver);
+    const result = browserIsSupported();
+    document.getElementById(MAIN_ID).hidden = !result;
 
-  // show warning message before unloading
-  window.addEventListener('beforeunload',
-      GameIframeObserver.showWarningBeforeUnload);
+    return result;
+  })() && (() => {
+    // Step 2: If the browser supports running the game, then
+    // initialize the iframe observers.
 
-  return browserIsSupported;
+    // initialize observers for AE1 and AE2
+    const ae1IframeObserver = new GameIframeObserver(
+        document.getElementById(AE1_GAME_IFRAME_ID),
+        GAME_URLS[GAMES.AE1]);
+    const ae2IframeObserver = new GameIframeObserver(
+        document.getElementById(AE2_GAME_IFRAME_ID),
+        GAME_URLS[GAMES.AE2]);
+    initGameIframeObservers(ae1IframeObserver, ae2IframeObserver);
+
+    // show warning message before unloading
+    window.addEventListener('beforeunload',
+        GameIframeObserver.showWarningBeforeUnload);
+
+    return true;
+  })() && (() => {
+    // Step 3: Initialize the fullscreen settings.
+
+    const fullscreenCheckboxObserver = new (
+      class FullscreenCheckboxObserver extends Observer {
+        /** @param { HTMLInputElement } checkbox */
+        constructor(checkbox) {
+          super();
+
+          this.checkbox = checkbox;
+
+          checkbox.checked = getFullscreenStatus();
+          checkbox.addEventListener('input',
+              FullscreenCheckboxObserver.inputListener);
+        }
+
+        /** @override */
+        receive(value) {
+          this.checkbox.checked = Boolean(value);
+        }
+
+        /** @param { Event } e */
+        static inputListener(e) {
+          setFullscreenStatus(Boolean(e.target.checked));
+        }
+      }
+    )(document.getElementById(FULLSCREEN_SETTINGS_TOGGLE_ID));
+
+    initFullscreenSettings(fullscreenCheckboxObserver);
+
+    return true;
+  })();
 };
 
-/** Sync full screen checkbox status with settings. */
-class FullscreenCheckboxObserver extends Observer {
-  /** @param { HTMLInputElement } checkbox */
-  constructor(checkbox) {
-    super();
-
-    this.checkbox = checkbox;
-
-    checkbox.checked = getFullscreenStatus();
-    checkbox.addEventListener('input',
-        FullscreenCheckboxObserver.inputListener);
-  }
-
-  /** @override */
-  receive(value) {
-    this.checkbox.checked = Boolean(value);
-  }
-
-  /** @param { Event } e */
-  static inputListener(e) {
-    setFullscreenStatus(Boolean(e.target.checked));
-  }
-}
-
-/**
- * Initialize fullscreen settings.
- * @return { boolean } `true`
- */
-const initFullscreen = () => {
-  // use the checkbox on the page to set fullscreen status
-  const checkbox = document.getElementById(FULLSCREEN_SETTINGS_TOGGLE_ID);
-  // add a checkbox observer to sync with the page's fullscreen settings
-  const checkboxObserver = new FullscreenCheckboxObserver(checkbox);
-
-  initFullscreenSettings(checkboxObserver);
-
-  return true;
-};
-
-document.addEventListener('DOMContentLoaded', () => {
-  return initIndexUi() && initFullscreen();
-});
+document.addEventListener('DOMContentLoaded', init);
